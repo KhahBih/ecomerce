414abbf4c1abcd0591da6795bb3ede0de1066aef
KhahBih <www.kbinh@gmail.com>
2023-11-12 17:09:07 +0700

    12-11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {createBrowserRouter, Router, Routes, Route, RouterProvider} from 'react-router-dom';
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/auth/Home";
+
 
 function App() {
+  // ROUTER OPTION 
+  // var router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Root />,
+  //   },
+  // ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/register" element={<Register />}/>
+        <Route exact path="/login" element={<Login />}/>
+      </Routes>
+    </>
   );
 }
 
