f2cb06d6c37a71f02254df550553187affe2b99e
KhahBih <www.kbinh@gmail.com>
2023-11-23 22:28:35 +0700

    23-11

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {createBrowserRouter, Router, Routes, Route, RouterProvider} from 'react-router-dom';
-import {ToastContainer} from 'react-toastify'
+import {ToastContainer} from 'react-toastify';
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
 import 'react-toastify/dist/ReactToastify.css';
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -10,6 +12,23 @@ import RegisterComplete from "./pages/auth/RegisterComplete";
 
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+        if(user){
+          const idTokenResult = await user.getIdTokenResult()
+          console.log("user",user);
+          dispatch({
+            type: 'LOGGED_IN_USER',
+            payload: {
+              name: 'shiro'
+            }
+        })
+      }
+    })
+    return () => unsubscribe()
+  },[])
+  
   return (
     <>
       <Header />
